732a94d8e1c1a22139ac6467e4af3b729a010332
aliyafarkhshatova <liyafarapp@gmail.com>
2025-10-20 13:19:37 +0500

    fix  issues  saving in temp directory

--- ios/Runner/NativeBridge.mm
+++ ios/Runner/NativeBridge.mm
@@ -9,7 +9,7 @@ extern "C" const char* writeHelloAndReturnContent(int n, const char* basePath) {
     std::string path = std::string(basePath) + "/hello.txt";
 
     std::ofstream file(path, std::ios::out | std::ios::trunc);
-    file << "hello world " << n  << std::endl;
+    file << "hello world " <<  n  << std::endl;
     file.close();
 
     std::ifstream reader(path);
